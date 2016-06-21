5b08b1a8e5fc1f052e50678f154ed58ae85cc513
scott <stsmall@gmail.com>
2016-06-21 17:20:09 -0400

    f

--- mtTree.py
+++ mtTree.py
@@ -102,11 +102,11 @@ class mtTree:
 #            command = self.bwa + " mem -t " + str(self.threads) + " " + reference + " " + self.fq1 + " " + self.fq2 + " > tmp.sam"
 #            proc = subprocess.Popen(command, shell=True)
 #            proc.wait()
-        command = self.bowtie2 + "-build -f" + reference
+        command = self.bowtie2 + "/bowtie2-build -f" + reference
         proc = subprocess.Popen(command, shell=True)
         proc.wait()  
 
-        command = self.bowtie2 + " -p " + str(self.threads) + " --no-unal -R 5 -N 1 -L 12 -D 25 -i S,2,.25 -x " + reference + " -1 " + self.fq1 + " -2 " + self.fq2 + " -S tmp.sam"
+        command = self.bowtie2 + "/bowtie2 -p " + str(self.threads) + " --no-unal -R 5 -N 1 -L 12 -D 25 -i S,2,.25 -x " + reference + " -1 " + self.fq1 + " -2 " + self.fq2 + " -S tmp.sam"
         proc = subprocess.Popen(command, shell=True)
         proc.wait()     
     
