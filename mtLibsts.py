522491d806148956761be11d96e01bec8202901b
scott <stsmall@gmail.com>
2016-06-23 13:50:35 -0400

    f

--- mtLibsts.py
+++ mtLibsts.py
@@ -49,7 +49,8 @@ def sam_2_pe(samfile,pe1,pe2):
     if out.split("\n")[0].split("\t")[0] in out.split("\n")[1].split("\t")[0]:  
         
         #Extract forward reads
-        command = "cat " + samfile + ''' | grep -v ^@ | awk 'NR%2==1 {print "@"$1"\n"$10"\n+\n"$11}' > ''' + pe1 
+        subcommand = ''' | grep -v ^@ | awk 'NR%2==1 {print "@"$1"\n"$10"\n+\n"$11}' > ''' 
+        command = "cat " + samfile + subcommand + pe1 
         print command        
         proc = subprocess.Popen(command,shell=True)
         proc.wait()
